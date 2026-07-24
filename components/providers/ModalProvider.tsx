f6240619d4b2d6d596653e0d925b404376f8d368
dytsou <d.y.tsou@gmail.com>
2026-07-24 17:01:31 +0800

    fix(sonar): Add a "type" attribute to this button.
    Add an explicit "type" attribute to this button.

--- components/providers/ModalProvider.tsx
+++ components/providers/ModalProvider.tsx
@@ -270,6 +270,7 @@ export const ModalProvider = ({ children }: { children: ReactNode }) => {
                 {promptState.options.cancelText || "Batal"}
               </button>
               <button
+                type="button"
                 className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2"
                 onClick={() => handlePromptClose(promptState.inputValue)}
               >
