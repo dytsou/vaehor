702dad7749d145ee9b13d012b4bc55fde4cd7c9b
dytsou <d.y.tsou@gmail.com>
2026-07-24 16:16:05 +0800

    fix(sonar): Prefer `childNode.remove()` over `parentNode.removeChild(childNode)`.
    Prefer `childNode.remove()` over `parentNode.removeChild(childNode)`.

--- components/file-details/InfoPanel.tsx
+++ components/file-details/InfoPanel.tsx
@@ -364,7 +364,7 @@ export default function InfoPanel({
             iframe.src = directLink;
             document.body.appendChild(iframe);
             setTimeout(() => {
-              document.body.removeChild(iframe);
+              document.iframe.remove();
             }, 5000);
           }}
           className="flex items-center justify-center px-4 py-3 bg-primary text-primary-foreground rounded-lg hover:bg-primary/90 font-semibold"
