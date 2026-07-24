5040a9ab985c66417f5d615ab5d214f1f76c1ef4
dytsou <d.y.tsou@gmail.com>
2026-07-24 16:02:43 +0800

    fix(sonar): Prefer `Number.parseFloat()` over `parseFloat()`.
    Prefer `node:fs/promises` over `fs/promises`.

--- lib/storage/local.ts
+++ lib/storage/local.ts
@@ -1,4 +1,4 @@
-import fs from "fs/promises";
+import fs from "node:fs/promises";
 import path from "path";
 import { getMimeType } from "./mime";
 import { ZeeFile, ListFilesResponse } from "@/types/storage";
